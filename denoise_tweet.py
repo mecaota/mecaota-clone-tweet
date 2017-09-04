e43da1c1cbe30ba89d2205b5769d7f5ae7ff96d0
mecaota <mecaota@pmc02tt38xhv2n.pepabo.private>
2017-09-04 14:55:51 +0900

    各処理部にコメント追加

--- denoise_tweet.py
+++ denoise_tweet.py
@@ -4,11 +4,14 @@ import numpy
 import sys
 
 args = sys.argv
+
+# csv import
 tweets = pandas.read_csv(args[1],encoding="utf-8")
-print(len(tweets))
 
-
+# in_reply_to_status_idとin_reply_to_user_idの値が入っているツイートを除外
 tweets = tweets[tweets["in_reply_to_status_id"].isnull() | tweets["in_reply_to_user_id"].isnull()]
+
 print(tweets)
 
+# csv export
 tweets.to_csv("tweets_shaped.csv")
