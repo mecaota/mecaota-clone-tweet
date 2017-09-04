a452ff6d7228e0a682dc03b34f858e8b7c3a4519
mecaota <mecaota@pmc02tt38xhv2n.pepabo.private>
2017-09-04 14:52:28 +0900

    reply系idが付与されたツイートを弾く処理実装

--- denoise_tweet.py
+++ denoise_tweet.py
@@ -5,4 +5,10 @@ import sys
 
 args = sys.argv
 tweets = pandas.read_csv(args[1],encoding="utf-8")
+print(len(tweets))
+
+
+tweets = tweets[tweets["in_reply_to_status_id"].isnull() | tweets["in_reply_to_user_id"].isnull()]
 print(tweets)
+
+tweets.to_csv("tweets_shaped.csv")
