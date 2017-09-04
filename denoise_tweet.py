9d0734c6994e5e71c7ac503b40a2daf793b015ec
mecaota <mecaota@pmc02tt38xhv2n.pepabo.private>
2017-09-04 17:33:48 +0900

    送信元クライアントを元にノイズツイートを弾く処理実装。ちゃっかりDSStoreファイルをgit除外

--- denoise_tweet.py
+++ denoise_tweet.py
@@ -6,6 +6,7 @@ import sys
 args = sys.argv
 
 # csv import
+print(args[1])
 tweets = pandas.read_csv(args[1],encoding="utf-8")
 
 # in_reply_to_status_idとin_reply_to_user_idの値が入っているツイートを除外
@@ -14,7 +15,21 @@ tweets = tweets[tweets["in_reply_to_status_id"].isnull() | tweets["in_reply_to_u
 # RTされたツイート「RT @」で始まるツイートを除外
 tweets = tweets[~tweets['text'].str.contains('RT.@*')]
 
-print(tweets)
+# ツイート元クライアントを指定
+exc = ['Foursquare','TwitCasting','Periscope','niconico','ツイ廃','Twitter.Web']
+inc = ['Android','TheWorld','Twitter','niconico','SobaCha','TweetDeck','Tweetbot','tw','ツイタマ','pictureadd','erased1023935','ATOK.Pad','SekaiCameraZoom','Hel1','Janetter','MateCha','ニコニコニュース']
+# ブラックリスト処理
+for i in exc:
+    tweets = tweets[~tweets['source'].str.contains(i)]
+filtered = tweets[~tweets['text'].str.contains('')]
+
+# ホワイトリスト処理
+for i in inc:
+    filtered = pandas.concat([filtered,tweets[tweets['source'].str.contains(i)]])
+# データセットとして使うデータ列を選択
+tweets = filtered[['text','timestamp']]
+
+# print(tweets)
 
 # csv export
 tweets.to_csv("tweets_shaped.csv")
