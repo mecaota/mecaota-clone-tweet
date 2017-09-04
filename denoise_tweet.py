08cea3a6b0619df5e8d2aa3d7dfb43e55b9bb099
mecaota <mecaota@pmc02tt38xhv2n.pepabo.private>
2017-09-04 18:26:37 +0900

    モジュール化してリプと通常ツイを別々に処理できるようにしようとして失敗

--- denoise_tweet.py
+++ denoise_tweet.py
@@ -3,33 +3,47 @@ import pandas
 import numpy
 import sys
 
-args = sys.argv
+# in_reply_to_status_idとin_reply_to_user_idの値が入っているツイートを選別。０ならリプライ除外、それ以外はリプライのみ
+def tweet_filter(tweets, mode=0):
+    if(mode == 0):
+        tweets = tweets[tweets["in_reply_to_status_id"].isnull() | tweets["in_reply_to_user_id"].isnull()]
+    else:
+        tweets = tweets[tweets["in_reply_to_status_id"].notnull() | tweets["in_reply_to_user_id"].notnull()]
+        tweets = tweets.drop(["in_reply_to_status_id", "in_reply_to_user_id"], axis=1)
+    return _denoise(tweets)
 
-# csv import
-print(args[1])
-tweets = pandas.read_csv(args[1],encoding="utf-8")
+def _denoise(tweets):
+    # RTされたツイート「RT @」で始まるツイートを除外
+    tweets = tweets[~tweets['text'].str.contains('RT.@*')]
 
-# in_reply_to_status_idとin_reply_to_user_idの値が入っているツイートを除外
-tweets = tweets[tweets["in_reply_to_status_id"].isnull() | tweets["in_reply_to_user_id"].isnull()]
+    # ツイート元クライアントを指定
+    exc = ['Foursquare','TwitCasting','Periscope','niconico','ツイ廃','Twitter.Web']
+    inc = ['Android','TheWorld','Twitter','niconico','SobaCha','TweetDeck','Tweetbot','tw','ツイタマ','pictureadd','erased1023935','ATOK.Pad','SekaiCameraZoom','Hel1','Janetter','MateCha','ニコニコニュース']
+    # ブラックリスト処理
+    for i in exc:
+        tweets = tweets[~tweets['source'].str.contains(i)]
+    filtered = tweets[~tweets['text'].str.contains('')]
 
-# RTされたツイート「RT @」で始まるツイートを除外
-tweets = tweets[~tweets['text'].str.contains('RT.@*')]
+    # ホワイトリスト処理
+    for i in inc:
+        filtered = pandas.concat([filtered,tweets[tweets['source'].str.contains(i)]])
+    # データセットとして使うデータ列を選択
+    tweets = filtered[['text','timestamp']]
 
-# ツイート元クライアントを指定
-exc = ['Foursquare','TwitCasting','Periscope','niconico','ツイ廃','Twitter.Web']
-inc = ['Android','TheWorld','Twitter','niconico','SobaCha','TweetDeck','Tweetbot','tw','ツイタマ','pictureadd','erased1023935','ATOK.Pad','SekaiCameraZoom','Hel1','Janetter','MateCha','ニコニコニュース']
-# ブラックリスト処理
-for i in exc:
-    tweets = tweets[~tweets['source'].str.contains(i)]
-filtered = tweets[~tweets['text'].str.contains('')]
+    return tweets
 
-# ホワイトリスト処理
-for i in inc:
-    filtered = pandas.concat([filtered,tweets[tweets['source'].str.contains(i)]])
-# データセットとして使うデータ列を選択
-tweets = filtered[['text','timestamp']]
 
-# print(tweets)
 
-# csv export
-tweets.to_csv("tweets_shaped.csv")
+if __name__ == "__main__":
+    args = sys.argv
+
+    # csv import
+    tweets = pandas.read_csv(args[1], encoding="utf-8")
+    tweets = tweet_filter(tweets, 0)
+    replies = tweet_filter(tweets, 0)
+
+    print(tweets)
+
+    # csv export
+    tweets.to_csv("tweets_shaped.csv")
+    repliess.to_csv("replies_shaped.csv")
