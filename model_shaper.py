5683e90ba3b3e9c880fd277bf3d58b67df98a95d
mecaota <mecaota@pmc02tt38xhv2n.pepabo.private>
2017-09-05 11:33:01 +0900

    ベクトル化処理をvectrize関数を設置して関数化

--- model_shaper.py
+++ model_shaper.py
@@ -4,6 +4,8 @@ import re
 import numpy
 import sys
 
+#define STR_MAX = 20
+
 def load_csv(path,mode = 0):
     # normal Tweet用csv読み込み処理
     if mode == 0:
@@ -19,15 +21,21 @@ def load_csv(path,mode = 0):
     print(path)
     return pandas.read_csv(path,encoding="utf-8")
 
+def vectrize(tweets):
+    for i in tweets[1]:
+        print(i)
+    return tweets
+
+
 if __name__ == "__main__":
-    args = ["",""]
+    dir = ""
 
     # load_csv <0:RTtweet,0:normaltweet,0<:reply
-    #rts = load_csv(args[1], -1)
-    tweets = load_csv(args[1], 0)
-    replies = load_csv(args[1], 1)
-
+    #rts = load_csv(dir, -1)
+    tweets = load_csv(dir, 0)
+    replies = load_csv(dir, 1)
     
+    vector_tweets = vectrize(tweets)
 
-    # csv export
-    print(tweets)
+    # csv output
+    print(vector_tweets)
