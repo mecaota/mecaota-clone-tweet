cf5c01892406e2a3437a56c1aedf47f26a214b55
mecaota <mecaota@pmc02tt38xhv2n.pepabo.private>
2017-09-06 15:36:47 +0900

    モデル作成途中でpush

--- machine_learning.py
+++ machine_learning.py
@@ -11,17 +11,59 @@ import model_shaper as m_shape
 def create_model(X, Y):
     epochs = 2
     batch_size = X.shape[0] * X.shape[1] * X.shape[2]
-    print(X.shape)
     model = Sequential()
-    model.add(LSTM(64, return_sequences=True, input_dim=X.shape[2], input_length=X.shape[1]))
-    model.add(LSTM(32, return_sequences=True))
+    model.add(LSTM(64, return_sequences=False, input_shape=(X.shape[1], X.shape[2])))
+    #model.add(LSTM(64, return_sequences=True, input_shape=(20, X.shape[2])))
+    model.add(LSTM(32, return_sequences=False))
     model.add(Dense(20))
     model.add(Activation("linear"))
     model.compile(loss = 'mean_squared_error', optimizer = 'adam')
     return model
 
-def learning(model):
-    return
+def sampling(preds, temperature=1.0):
+    # helper function to sample an index from a probability array
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds) / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
+
+def learning(model, X, Y):
+    # train the model, output generated text after each iteration
+    for iteration in range(1, 60):
+        print()
+        print('-' * 50)
+        print('Iteration', iteration)
+        model.fit(X, Y, batch_size=128, epochs=1)
+
+        start_index = random.randint(0, len(text) - maxlen - 1)
+
+        for diversity in [0.2, 0.5, 1.0, 1.2]:
+            print()
+            print('----- diversity:', diversity)
+
+            generated = ''
+            sentence = text[start_index: start_index + maxlen]
+            generated += sentence
+            print('----- Generating with seed: "' + sentence + '"')
+            sys.stdout.write(generated)
+
+            for i in range(140):
+                x = np.zeros((1, maxlen, len(chars)))
+                for t, char in enumerate(sentence):
+                    x[0, t, char_indices[char]] = 1.
+
+                preds = model.predict(x, verbose=0)[0]
+                next_index = sampling(preds, diversity)
+                next_char = indices_char[next_index]
+
+                generated += next_char
+                sentence = sentence[1:] + next_char
+
+                sys.stdout.write(next_char)
+                sys.stdout.flush()
+            print()
 
 if __name__ == "__main__":
     dir = ""
@@ -31,4 +73,4 @@ if __name__ == "__main__":
     replies = m_shape.load_csv(dir, 1)
     X, Y = m_shape.labering(tweets)
     model = create_model(X, Y)
-    result = learning(model)
+    result = learning(model, X, Y)
