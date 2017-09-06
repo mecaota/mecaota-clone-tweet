15ce0b23c7507a23a9f8d7206c72e5f494f08ee7
mecaota <mecaota@pmc02tt38xhv2n.pepabo.private>
2017-09-06 10:58:45 +0900

    create machine_learning.py

---
+++ machine_learning.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+from keras.layers import Dense, Activation
+from keras.models import Sequential
+
+import model_shaper
+
+if __name__ == "__main__":
+
+    return
