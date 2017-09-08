04855507914a152c87337908ec3190ab6e75c2c1
mecaota <b1014167@gmail.com>
2017-09-08 18:03:27 +0900

    Revert "漢字・カタカナをひらがなに変換する実装"

--- machine_learning.py
+++ machine_learning.py
@@ -12,9 +12,11 @@ from keras.callbacks import ModelCheckpoint
 
 import TweetDataset
 
-def create_model(batch_size, output_dim):
+def create_model(X, Y):
     print("model create task processing,,,")
     epochs = 2
+    batch_size = X.shape[1]
+    output_dim = X.shape[2]
     model = Sequential()
     model.add(LSTM(64, return_sequences=False, input_shape=(batch_size, output_dim)))
     model.add(Dense(output_dim, activation='relu'))
@@ -34,18 +36,15 @@ def learning(model, dataset):
     # train the model, output generated text after each iteration
     X = dataset["X"]
     Y = dataset["Y"]
-    maxlen = X.shape[1]
-    text = ""
-    for i in X[0]:
-        text += str(i)
+    maxlen = dataset["strmax"]
+    text = dataset["alltweet"]
     chars = dataset["chars"]
-    print(X[2])
     char_indices = dataset["char_indices"]
     indices_char = dataset["indices_chars"]
     cb = None
 
 
-    for iteration in range(1, 60):
+    for iteration in range(1, 2):
         print()
         print('-' * 50)
         print('Iteration', iteration)
@@ -102,7 +101,7 @@ def open_model_dataset(systemcall):
     except (FileNotFoundError, OSError):
         print("model create from dataset")
         dataset = TweetDataset.TweetDataset("tweetdata/" + filename + "_shaped.csv")
-        model = create_model(dataset.X.shape[1], dataset.X.shape[2])
+        model = create_model(dataset.X, dataset.Y)
         dataset_dict = dataset.to_dict()
 
     model.summary()
